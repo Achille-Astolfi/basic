624c709b019f447eda471254aa2e90cb23b2b81c
stefa <stefa@DESKTOP-MGMDIO4>
2020-04-21 17:03:06 +0200

    commit versione 2!!

--- src/main/java/com/example/basic/StefanoPozzobon.java
+++ src/main/java/com/example/basic/StefanoPozzobon.java
@@ -2,10 +2,18 @@ package com.example.basic;
 
 /**
  * 
- * @author Stefano
- * @version 1 
+ * @author Stefano Pozzobon
+ * @version 2 
  * @since 1
  *
  */
 public class StefanoPozzobon {
+	/**
+	 * @since 2
+	 * @return
+	 */
+	public String greet() {
+		return "Hello Guys!";
+	}
 }
+
