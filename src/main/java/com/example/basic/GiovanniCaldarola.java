104fbe4e74460df8ef75a91786fd73d2bd0de297
giovanni <giovanni@localhost>
2020-04-21 17:03:03 +0200

    .

--- src/main/java/com/example/basic/GiovanniCaldarola.java
+++ src/main/java/com/example/basic/GiovanniCaldarola.java
@@ -2,9 +2,14 @@ package com.example.basic;
 
 /**
  * 
- * @author giovanni
- * @version 1
+ * @author giovanni caldarola
+ * @version 2
  */
 public class GiovanniCaldarola {
-
+	
+	private String msg;
+	
+	public String getMsg() {
+		return this.msg;
+	}
 }
