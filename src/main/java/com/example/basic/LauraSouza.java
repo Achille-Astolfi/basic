cd9e3b2a843044e07f63176b712d853c8260d5df
l_das <lauradealmeidasouza123@gmail.com>
2020-04-21 17:20:05 +0200

    .

--- src/main/java/com/example/basic/LauraSouza.java
+++ src/main/java/com/example/basic/LauraSouza.java
@@ -3,7 +3,7 @@ package com.example.basic;
 /**
  * 
  * @author lauradealmeidasouza
- * @version 2
+ * @version 3
  * @since 1 
  *
  */
@@ -13,5 +13,6 @@ public class LauraSouza {
 	public String hiGuys() {
 		return "Hi Guys";
 	}
+
 
 }
