744ab4c8833844bdfd4f60d8c5c468cfdb8dad93
Jacopo Foti <13jacopo@gmail.com>
2020-04-22 15:18:54 +0200

    .

--- src/main/java/com/example/basic/serviceimpl/SplitServiceImpl.java
+++ src/main/java/com/example/basic/serviceimpl/SplitServiceImpl.java
@@ -5,11 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.basic.service.SplitService;
 
-@RestController
 public class SplitServiceImpl implements SplitService{
 
 @Autowired
