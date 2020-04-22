d5647195ac9ada076f64e9210b3c67ded8402ff1
Petrut Cabulca <Petrut Cabulca petrut.cabulca@example.com>
2020-04-22 14:47:37 +0200

    .

--- src/main/java/com/example/basic/controller/SquareController.java
+++ src/main/java/com/example/basic/controller/SquareController.java
@@ -5,18 +5,11 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.basic.dto.JoinDto;
-import com.example.basic.dto.SquareDto;
-import com.example.basic.dto.SumDto;
+
 import com.example.basic.protocol.SquareProtocol;
-import com.example.basic.protocol.SumProtocol;
 import com.example.basic.service.SquareService;
 
 @RestController
