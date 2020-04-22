fea0cd9bdbedd06e3a6ac367ed41e7869117f1da
Jacopo Foti <13jacopo@gmail.com>
2020-04-22 12:13:13 +0200

    Inserito SquareProtocol

---
+++ src/main/java/com/example/basic/protcol/SquareProtocol.java
@@ -0,0 +1,16 @@
+package com.example.basic.protcol;
+
+public class SquareProtocol {
+
+		/*square=value*value*/
+		private float square;
+		
+		//GETTERS AND SETTERS
+		
+		public float getSquare() {
+			return square;
+		}
+		public void setSquare(float square) {
+			this.square = square;
+		}
+}
