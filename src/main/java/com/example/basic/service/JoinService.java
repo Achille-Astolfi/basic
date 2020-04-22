4ec3fb5cc2b1fbb8edc989315c084330ed6b79e8
janli <janli@DESKTOP-H2OSPU4>
2020-04-22 14:44:44 +0200

    Implementazione di service e service impl

--- src/main/java/com/example/basic/service/JoinService.java
+++ src/main/java/com/example/basic/service/JoinService.java
@@ -2,6 +2,6 @@ package com.example.basic.service;
 
 public abstract interface JoinService {
 	
-	String string(String string1, String string2);
+	String joinString(String string1, String string2);
 
 }
