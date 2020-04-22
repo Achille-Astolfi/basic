bb1c6909e60a59606a357c0429dc62f5f3b0c74d
Jacopo Foti <13jacopo@gmail.com>
2020-04-22 17:34:48 +0200

    .

--- src/main/java/com/example/basic/protocol/SquareProtocol.java
+++ src/main/java/com/example/basic/protocol/SquareProtocol.java
@@ -1,17 +1,12 @@
 package com.example.basic.protocol;
 
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
-import com.example.basic.controller.SquareController;
 // COMMENTO FITTIZIO
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SquareProtocol extends ResourceSupport{
 
-		public SquareProtocol() {
-			this.add(ControllerLinkBuilder.linkTo(SquareController.class).withSelfRel());
-		}
 	
 		@JsonProperty("square")
 		private double square;
